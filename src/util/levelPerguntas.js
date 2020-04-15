baa33c38d01f47c48b154603ebb4fc4a1fad4fef
Alécio <alecio18@hotmail.com>
2020-04-15 16:03:02 -0300

    adicionado array titulo perguntas

---
+++ src/util/levelPerguntas.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        pergunta: 'valendo mil',        
+    }, 
+    {
+        pergunta: 'valendo 2 mil',        
+    },  
+    {
+        pergunta: 'valendo 3 mil',        
+    },  
+    {
+        pergunta: 'valendo 4 mil',        
+    },  
+    {
+        pergunta: 'valendo 5 mil',        
+    },  
+    {
+        pergunta: 'valendo 10 mil',        
+    }, 
+    {
+        pergunta: 'valendo 20 mil',        
+    }, 
+    {
+        pergunta: 'valendo 30 mil',        
+    }, 
+    {
+        pergunta: 'valendo 40 mil',        
+    }, 
+    {
+        pergunta: 'valendo 50 mil',        
+    },
+    {
+        pergunta: 'valendo 100 mil',        
+    }, 
+    {
+        pergunta: 'valendo 200 mil',        
+    },
+    {
+        pergunta: 'valendo 300 mil',        
+    },
+    {
+        pergunta: 'valendo 400 mil',        
+    },
+    {
+        pergunta: 'valendo 500 mil',        
+    },
+    {
+        pergunta: 'valendo 1 milhão',        
+    },
+]
